568bb059b1b7b0d42b1eaab594a5a98642b8bdd0
Aluno <aluno@etec.sp.gov.br>
2023-11-16 22:52:44 -0300

    Atividades Aula 16/11/2023

---
+++ PA/Aula_16_11_2023_Ex01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula_16_11_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int todaIdadeInformada = 0;
+            int somaIdade = 0;
+
+
+            for (int i = 1; i > 0; i++)
+            {
+                Console.Write("Digite uma idade: ");
+                idade = int.Parse(Console.ReadLine());
+
+                if (idade == 0)
+                {
+                    todaIdadeInformada = i - 1;
+                    break;
+                }
+
+                somaIdade += idade;
+            }
+
+            Console.WriteLine($"Média das idades informada é: {somaIdade / todaIdadeInformada}");
+        }
+    }
+}
